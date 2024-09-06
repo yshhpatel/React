d789c8efeab1377bb3c17d36332a819ebab38920
yshhpatel <162433826+yshhpatel@users.noreply.github.com>
2024-09-06 12:48:27 +0530

    Add files via upload

---
+++ React Events and Conditional Rendering/src/Components/Condition.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+export default function Condition() {
+    const [visible, setvisible] = useState(false)
+    const [vis, setvis] = useState(true)
+    return (
+        <div>
+            <h1>Condition1</h1>
+            {
+                visible ? <h1>Hello</h1> : <h1>World</h1>
+            }
+            <button onClick={() => {
+                setvisible(!visible)
+            }}>Click</button>
+            <h1>Condition2</h1>
+            {
+                vis && <h1>Hello</h1>
+            }
+            <button onClick={() => {
+                setvis(!vis)
+            }}>Click2</button>
+        </div>
+    )
+}
