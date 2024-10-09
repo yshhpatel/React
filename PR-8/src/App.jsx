90ba87cf2d5f0eb0816c39a8b99839eb9769fbe3
yshhpatel <162433826+yshhpatel@users.noreply.github.com>
2024-10-09 12:24:13 +0530

    Add files via upload

---
+++ PR-8/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Index from './Components/Index'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Index/>
+      </div>
+    )
+  }
+}
